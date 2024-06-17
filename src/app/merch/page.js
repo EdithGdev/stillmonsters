79b570206979ff37b9d96920bb2338e1f7ea22ce
EdithGdev <edithgonzalez9630@gmail.com>
2024-06-17 14:39:39 -0700

    added styles sheet to merch page

--- src/app/merch/page.js
+++ src/app/merch/page.js
@@ -1,8 +1,8 @@
-
+import styles from "./merch.module.css"
 
 export default function Merch() {
   return (
-    <main>
+    <main className={styles.main}>
       <h2>Merch page!</h2>
     </main>
   );
